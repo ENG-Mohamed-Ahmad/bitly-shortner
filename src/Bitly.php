a462e7ee7718d1327786c061b211c0c70da6d5d7
hamo332 <immuslim844@gmail.com>
2024-06-20 18:48:01 +0300

    initial commit

---
+++ src/Bitly.php
@@ -0,0 +1,56 @@
+<?php
+namespace Mohamed\BityShortner;
+
+class Bitly
+{
+
+    public static function shortner($long_link = ''){
+
+        if (empty($long_link)) {
+            throw new \Exception('Please Provide Long Link');
+        }
+        config('bitlyconfig.token')
+        if (is_null($token == getBitlyToken())) {
+            
+            throw new \Exception('Please Provide Token in bitlyconfig.php file');
+        }
+
+        $requestBody = json_encode(
+            [
+                    "domain"=> "bit.ly",
+                    "long_url"=> $long_link,
+                ]
+            );
+
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://api-ssl.bitly.com/v4/shorten',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => $requestBody,
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Bearer '. $token,
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+
+        $response = json_decode($response);
+
+        return isset($response) ? $response->link : '';
+    }
+}
+
+?>
+
+
+?>
---
+++ src/BitlyServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mohamed\BityShortner;
+
+use Carbon\Laravel\ServiceProvider;
+
+class BitlyServiceProvider extends ServiceProvider
+{
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/config/bitlyconfig.php' => config_path('bitlyconfig.php'),
+        ]);
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/bitlyconfig.php', 'bitlyconfig'
+        );
+    }
+}
+
+
+?>
